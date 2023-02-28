06537cd6116dd8e10bacb649ec2f75074c54cd3a
b14-10t <b14-10t@AULAB14-PC10.CICLOS.local>
2023-02-28 20:56:18 +0100

    yocarlos

--- PracticaGithub/src/paquete1/ClasePaquete1.java
+++ PracticaGithub/src/paquete1/ClasePaquete1.java
@@ -4,6 +4,7 @@ public class ClasePaquete1 {
 
 	public static void main(String[] args) {
 		System.out.println("");
+		System.out.println("");
 		System.out.println("COMMIT SOBRE LA CLAsSE 1");
 		System.out.println("HOLA MUNDO ESTA ES UNA PRACTICA PARA ENDES");
 	}
