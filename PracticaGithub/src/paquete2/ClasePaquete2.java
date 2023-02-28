487c3fce6aa5e9c8f8d7f4ba3359c050148910c6
b14-10t <b14-10t@AULAB14-PC10.CICLOS.local>
2023-02-28 20:39:32 +0100

    cambio pc 2

--- PracticaGithub/src/paquete2/ClasePaquete2.java
+++ PracticaGithub/src/paquete2/ClasePaquete2.java
@@ -4,6 +4,7 @@ public class ClasePaquete2 {
 
 	public static void main(String[] args) {
 		System.out.println("PRACTICA DE GITHUB EN LA CLASE 2");
+		System.out.println("esto es un cambio desde el pc 2");
 	}
 
 }
