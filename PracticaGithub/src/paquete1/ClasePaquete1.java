5f34715b663178f37652209695428060ac9fa88c
b14-12t <b14-12t@AULAB14-PC12.CICLOS.local>
2023-02-28 19:51:36 +0100

    asda

---
+++ PracticaGithub/src/paquete1/ClasePaquete1.java
@@ -0,0 +1,9 @@
+package paquete1;
+
+public class ClasePaquete1 {
+
+	public static void main(String[] args) {
+		System.out.println("HOLA MUNDO ESTA ES UNA PRACTICA PARA ENDES");
+	}
+
+}
---
+++ PracticaGithub/src/paquete2/ClasePaquete2.java
@@ -0,0 +1,9 @@
+package paquete2;
+
+public class ClasePaquete2 {
+
+	public static void main(String[] args) {
+		System.out.println("PRACTICA DE GITHUB EN LA CLASE 2");
+	}
+
+}
