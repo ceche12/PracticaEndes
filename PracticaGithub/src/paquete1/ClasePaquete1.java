fb44675d50af86525fe3671cd0827351da181b61
b14-12t <b14-12t@AULAB14-PC12.CICLOS.local>
2023-02-28 20:48:29 +0100

    asdas

--- PracticaGithub/src/paquete1/ClasePaquete1.java
+++ PracticaGithub/src/paquete1/ClasePaquete1.java
@@ -3,8 +3,9 @@ package paquete1;
 public class ClasePaquete1 {
 
 	public static void main(String[] args) {
-		System.out.println("COMMIT SOBRE LA CLAsSE 1");
+		System.out.println("COMMIT SOBRE LA CLASE 1");
 		System.out.println("HOLA MUNDO ESTA ES UNA PRACTICA PARA ENDES");
+		System.out.println("a");
 	}
 
 	
